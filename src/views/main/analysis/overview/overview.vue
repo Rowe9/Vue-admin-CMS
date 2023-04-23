11bbcf0aa5a4cd8a0670b9469d45c1299532fa29
Rowe9 <85985861+Rowe9@users.noreply.github.com>
2023-04-23 20:18:19 +0800

    Update overview.vue

--- src/views/main/analysis/overview/overview.vue
+++ src/views/main/analysis/overview/overview.vue
@@ -1,15 +1,14 @@
 <template>
   <div class="overview">
     <el-card class="box-card" :header="header">
-      <ul>
+     <ul>
         <li>开发工具：<span>Visual Studio Code</span></li>
         <li>编程语言：<span>TypeScript 4.x + JavaScript</span></li>
         <li>构建工具：<span>Vite2.x / Webpack5.x</span></li>
         <li>前端框架：<span>Vue3.x</span></li>
         <li>路由工具：<span>Vue Router4.x</span></li>
         <li>状态管理：<span>Pinia</span></li>
-        <li>可视化：<span>Element Plus</span></li>
-        <li>富文本：<span>WangEditor</span></li>
+        <li>可视化：<span>Echarts</span></li>
         <li>工具库：<span>@vueuse/core + dayjs + countup.js</span></li>
         <li>CSS预编译：<span>Sass / Less</span></li>
         <li>HTTP工具：<span>Axios</span></li>
