c4e9f6c10ba537d62f12ad486377720481f28b28
Rowe9 <85985861+Rowe9@users.noreply.github.com>
2023-04-27 20:33:22 +0800

    样式修改

--- src/views/main/analysis/overview/overview.vue
+++ src/views/main/analysis/overview/overview.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="overview">
-    <el-card class="box-card" :header="header">
-     <ul>
+    <el-card class="box-card1" :header="header1">
+      <div class="content">
+        Vue3Admin基于Vue3、Pinia、VueRouter、Vite、ElementPlus、Typescript、Echart5等后台系统解决方案
+      </div>
+    </el-card>
+    <el-card class="box-card2" :header="header2">
+      <ul>
         <li>开发工具：<span>Visual Studio Code</span></li>
         <li>编程语言：<span>TypeScript 4.x + JavaScript</span></li>
         <li>构建工具：<span>Vite2.x / Webpack5.x</span></li>
         <li>前端框架：<span>Vue3.x</span></li>
-        <li>路由工具：<span>Vue Router4.x</span></li>
+        <li>路由工具：<span>Vue Router 4.x</span></li>
         <li>状态管理：<span>Pinia</span></li>
-        <li>可视化：<span>Echarts</span></li>
+        <li>UI框架：<span>Element Plus</span></li>
+        <li>可视化：<span>Echarts 5.x</span></li>
         <li>工具库：<span>@vueuse/core + dayjs + countup.js</span></li>
         <li>CSS预编译：<span>Sass / Less</span></li>
         <li>HTTP工具：<span>Axios</span></li>
@@ -20,17 +26,22 @@
 
 <script setup lang="ts">
 interface IProps {
-  header?: string
+  header1?: string
+  header2?: string
 }
 
 withDefaults(defineProps<IProps>(), {
-  header: '技术栈'
+  header1: '关于',
+  header2: '技术栈'
 })
 </script>
 
 <style lang="less" scoped>
 .overview {
-  .el-card {
+  .box-card1 {
+    margin-bottom: 18px;
+  }
+  .box-card2 {
     ul {
       padding: 10px;
     }
